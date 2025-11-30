4da22af173860f4491293372fe931ec151b4bd70
Nara <aurora.raa252@gmail.com>
2025-11-30 16:27:28 +0700

    Add Login class with basic structure and verifyLogin stub

---
+++ src/Login.cpp
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////
+//  Login.cpp
+//  Implementation of the Class Login
+//  Created on:      30-Nov-2025 4:22:31 PM
+//  Original author: moog
+///////////////////////////////////////////////////////////
+
+#include "Login.h"
+
+
+Login::Login(){
+
+}
+
+
+
+Login::~Login(){
+
+}
+
+
+
+
+
+bool Login::verifyLogin(){
+
+	return false;
+}
---
+++ src/Login.h
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////
+//  Login.h
+//  Implementation of the Class Login
+//  Created on:      30-Nov-2025 4:22:31 PM
+//  Original author: moog
+///////////////////////////////////////////////////////////
+
+#ifndef LOGIN_H
+#define LOGIN_H
+#include <string>
+using namespace std;
+
+class Login
+{
+
+public:
+	Login();
+	virtual ~Login();
+
+	bool verifyLogin();
+
+private:
+	string emailLogin;
+	string passLogin;
+
+};
+#endif // LOGIN_H
